41fdbbc1981383806175703972d00c7118973a09
Griffin Graham <Griffin.Graham@enmu.edu>
2025-04-30 19:49:40 -0600

    Adding functionality for the classes options.

---
+++ Part 4/util/validators.py
@@ -0,0 +1,10 @@
+def recordExists(conn, table, column, value):
+    curse = conn.cursor()
+    curse.execute(f"SELECT 1 FROM {table} WHERE {column} = ?", (value,))
+    return curse.fetchone() is not None
+
+def isPositiveInteger(value):
+    try:
+        return int(value) > 0
+    except (ValueError, TypeError):
+        return False
